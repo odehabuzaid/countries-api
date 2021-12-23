f83956e4348b27bc19b2aba615cb4c55acd61dad
Odeh <abuzaid163@hotmail.com>
2021-12-24 01:01:02 +0200

    test: api url translator & json exporter

--- test/test.js
+++ test/test.js
@@ -2,11 +2,13 @@
 
 const supertest = require('supertest');
 const mongoose = require("mongoose");
-
+const fs = require("fs");
 const { app } = require('../app')
+const { translator, jsonExporter } = require('../utils')
 const requestWithSupertest = supertest(app);
 
 beforeEach((done) => {
+
     mongoose.connect(process.env.ATLAS_CON_STR,
         { useNewUrlParser: true, useUnifiedTopology: true },
         () => done());
@@ -18,11 +20,30 @@ afterEach((done) => {
     });
 });
 
+
+
 describe('Home Endpoint', () => {
 
     it('GET / ', async () => {
         const res = await requestWithSupertest.get('/');
         expect(res.status).toEqual(200);
+
+    });
+
+});
+
+describe('Utils', () => {
+
+    it('API URL Translator', async () => {
+        const url = await translator.translate(process.env.COUNTRIES_API_URL);
+        expect(url).toEqual("https://restcountries.com/v3.1/all");
+
+    });
+
+    it('Json File exporter', async () => {
+        const dir = await jsonExporter.exporter({ "test": "tobe exported" }, "exported.json")
+
+        expect(fs.existsSync(dir)).toEqual(true);
 
     });
 
