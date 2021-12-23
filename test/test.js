04064dccb132281cb18b5415a0f7546d0dcb2b29
Odeh <abuzaid163@hotmail.com>
2021-12-23 09:06:50 +0200

    test: database connection test added

--- test/test.js
+++ test/test.js
@@ -1,8 +1,22 @@
 'use-strict'
 
-const { app } = require('../app')
 const supertest = require('supertest');
+const mongoose = require("mongoose");
+
+const { app } = require('../app')
 const requestWithSupertest = supertest(app);
+
+beforeEach((done) => {
+    mongoose.connect(process.env.ATLAS_CON_STR,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        () => done());
+});
+
+afterEach((done) => {
+    mongoose.connection.db.dropDatabase(() => {
+        mongoose.connection.close(() => done())
+    });
+});
 
 describe('Home Endpoint', () => {
 
