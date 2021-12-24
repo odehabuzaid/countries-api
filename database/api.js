abe89b54fe52edab243491aab7755271d2ad1b15
Odeh <abuzaid163@hotmail.com>
2021-12-24 05:34:34 +0200

    database: added - insert data from countries api into countries db collection - exporting response to countries.json

---
+++ database/api.js
@@ -0,0 +1,34 @@
+
+const Globe = require('./models/countries')
+const axios = require('axios')
+const { translator, jsonExporter } = require('../utils')
+
+const { cityModel } = require('./structure')
+
+exports.fillTheDataBaseFromTheApi = async () => {
+    const url = await translator.translate(process.env.COUNTRIES_API_URL);
+    let structuredModel;
+    await axios.get(url)
+        .then(response => {
+            jsonExporter.exporter(response.data, "countries.json")
+            structuredModel = response.data
+            jsonExporter.exporter(cityModel(structuredModel), "structured.json")
+            Globe.find({}, '-_id', (err, entity) => {
+                if (entity.length != 0) {
+                    console.log(`there is ${entity.length} country in the db`,)
+                    // update db from api response
+                    // ..
+                    return entity
+                } else {
+                    // filling the db with structured data
+                    Globe.insertMany(structuredModel, (err, doc) => {
+                        if (err) { console.log(err) }
+                    });
+                    console.log(`${structuredModel.length} country inserted`)
+                    return structuredModel
+                }
+            });
+        })
+        .catch(error => error)
+}
+
--- database/connect.js
+++ database/connect.js
@@ -1,13 +1,14 @@
 'use strict'
 
 const mongoose = require('mongoose')
-
+const { fillTheDataBaseFromTheApi } = require('./api')
 async function ConnectToDataBase (dbConnectionURI) {
   const connectionPromis = new Promise((resolve, reject) => {
     mongoose.connect(dbConnectionURI)
     mongoose.connection
       .once('open', () => {
         console.log('\x1b[33m%s\x1b[0m', '✨ Connected => 🦅 ')
+        fillTheDataBaseFromTheApi() // can be scheduled using Agenda.js to check for api updates
         resolve('Mongoose connected')
       })
       .on('error', (error) => reject(error))
