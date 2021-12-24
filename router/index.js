59f3464eedc5ea21a6e46c26c35fb47c2ba6c0e0
Odeh <abuzaid163@hotmail.com>
2021-12-24 17:19:31 +0200

    added routes and controllers

--- router/index.js
+++ router/index.js
@@ -3,9 +3,18 @@
 const express = require('express')
 const router = express.Router()
 
+const Controller = require('../controllers')
+
 router.get('/favicon.ico', (req, res) => res.send('🌏'))
 router.get('/', (req, res) =>
     res.send('<center><img src="https://i.ibb.co/7jLr7vf/server.gif" ></center>')
 )
+
+router.get('/countries', Controller.countries)
+
+router.get('/currencies', Controller.currencies)
+
+router.get('/download', Controller.downloadFile)
+
 
 module.exports = router
